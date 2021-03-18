0e36ef4f5c13a902bae2eb3adf40b0d43548a3d0
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-03-17 22:12:15 -0500

    Update roboticsGUI.py

--- roboticsGUI.py
+++ roboticsGUI.py
@@ -2,7 +2,10 @@ import PySimpleGUIWeb as sg
 sg.theme('Dark Blue 13')
 
 def open_window():
-    layout = [[sg.Text("New Window", key="new")]]
+    layout = [[sg.Text("You've entered:")],
+              [sg.Text("Height: ")],
+              [sg.Text("Width: ")],
+              [sg.Button("Back"), sg.Button("Close")]]
     window = sg.Window("Second Window", layout, web_debug=False, web_ip='0.0.0.0', web_port=8080)
     choice = None
     while True:
@@ -13,8 +16,8 @@ def open_window():
     window.close()
 def main():
     layout = [[sg.Text("How large is the working area?")],
-              [sg.Text("Height (meters)...... ")],
-              [sg.Text("Width (meters)....... ")],
+              [sg.Text("Height (meters)...... "), sg.In(key='-h-')],
+              [sg.Text("Width (meters)....... "), sg.In(key='-w-')],
               [sg.Button("Continue", key="open")]]
     window = sg.Window("Main Window", layout, web_debug=False, web_ip='0.0.0.0', web_port=8080)
     while True:
@@ -22,6 +25,10 @@ def main():
         if event == "Exit" or event == sg.WIN_CLOSED or event is None:
             break
         if event == "open":
+            height = values['-h-']
+            width = values['-w-']
+            print(height)
+            print(width)
             open_window()
         
     window.close()
