acb44c8526b7f56ab992208422f5f240bf75caec
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-03-17 21:56:38 -0500

    Update roboticsGUI.py

--- roboticsGUI.py
+++ roboticsGUI.py
@@ -1,4 +1,6 @@
 import PySimpleGUIWeb as sg
+sg.theme('Dark Grey 13')
+
 def open_window():
     layout = [[sg.Text("New Window", key="new")]]
     window = sg.Window("Second Window", layout, web_debug=False, web_ip='0.0.0.0', web_port=8080)
@@ -10,7 +12,10 @@ def open_window():
         
     window.close()
 def main():
-    layout = [[sg.Button("Open Window", key="open")]]
+    layout = [[sg.Text("How large is the working area?")],
+              [sg.Text("Height (meters) : ")],
+              [sg.Text("Width (meters)  : ")],
+              [sg.Button("Continue", key="open")]]
     window = sg.Window("Main Window", layout, web_debug=False, web_ip='0.0.0.0', web_port=8080)
     while True:
         event, values = window.read()
