c5540fb2f652f1506e9dea84a4d9763c65f7561f
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-03-24 17:52:05 -0500

    Update roboticsGUI.py

--- roboticsGUI.py
+++ roboticsGUI.py
@@ -3,21 +3,21 @@ import PySimpleGUIWeb as sg
 sg.theme('Dark Blue 13')
 
 def open_window():
-    grid = [[sg.Button('yo', size=(5, 2), key=(x0,0), pad=(0,0)) for x0 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x1,1), pad=(0,0)) for x1 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x2,2), pad=(0,0)) for x2 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x3,3), pad=(0,0)) for x3 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x4,4), pad=(0,0)) for x4 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x5,5), pad=(0,0)) for x5 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x6,6), pad=(0,0)) for x6 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x7,7), pad=(0,0)) for x7 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x8,8), pad=(0,0)) for x8 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x9,9), pad=(0,0)) for x9 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x10,10), pad=(0,0)) for x10 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x11,11), pad=(0,0)) for x11 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x12,12), pad=(0,0)) for x12 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x13,13), pad=(0,0)) for x13 in range(15)],
-            [sg.Button('yo', size=(5, 2), key=(x14,14), pad=(0,0)) for x14 in range(15)]]
+    grid = [[sg.Button('yo', size=(4, 2), key=(x0,0), pad=(0,0)) for x0 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x1,1), pad=(0,0)) for x1 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x2,2), pad=(0,0)) for x2 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x3,3), pad=(0,0)) for x3 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x4,4), pad=(0,0)) for x4 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x5,5), pad=(0,0)) for x5 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x6,6), pad=(0,0)) for x6 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x7,7), pad=(0,0)) for x7 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x8,8), pad=(0,0)) for x8 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x9,9), pad=(0,0)) for x9 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x10,10), pad=(0,0)) for x10 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x11,11), pad=(0,0)) for x11 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x12,12), pad=(0,0)) for x12 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x13,13), pad=(0,0)) for x13 in range(15)],
+            [sg.Button('yo', size=(4, 2), key=(x14,14), pad=(0,0)) for x14 in range(15)]]
 
     options = [[sg.Text('Test Element')],
                [sg.Text('Test Element 2')]]
@@ -26,8 +26,7 @@ def open_window():
               [sg.Text('Made by Luken Henness', justification="center", font='Any 16', auto_size_text='True')],
               [sg.Text('')],
               [sg.Frame('Inputs Inputs Inputs Inputs Inputs',[[
-                  sg.Column(grid),
-                  sg.Column(options)
+                  sg.Column(grid),  sg.Column(options)
                   ]]
               )]
               ]
