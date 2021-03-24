85fe26fb51d0712967d2f97a69fec5c8658f5646
root <lrhenness@hasecuritysolutions>
2021-03-24 02:20:44 +0000

    changing

--- roboticsGUI.py
+++ roboticsGUI.py
@@ -12,7 +12,7 @@ def open_window(height,width):
     layout = [[sg.Text("You've entered:")],
               [sg.Text('Height: ' + str(height)),  sg.Text('Width: ' + str(width)),  sg.Button("Clear"), sg.Button("Close")],
               [sg.Text('')],
-              [sg.Button('yo', size=(4, 2), key=(x,y), pad=(0,0)) for x in range(width) for y in range(height)]]
+              [sg.Button('yo', size=(5, 2), key=(x,y), pad=(0,0)) for x in range(width) for y in range(height)]]
     window = sg.Window("Second Window", layout, web_debug=False, web_ip='0.0.0.0', web_port=8080)
 
     #window['-output1-'].update('height variable goes here')
