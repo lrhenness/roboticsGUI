d751abff3d1705f1323f27c72a3ae3a4b2822c6a
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-04-07 19:10:17 -0500

    Update roboticsGUI.py

--- roboticsGUI.py
+++ roboticsGUI.py
@@ -88,9 +88,9 @@ def open_window():
         elif event == "Submit": # Create and send MySQL command and output to -debug- textbox
             # Input validation for the following inputs:
             # -r1_heading-
-            print('-r1_heading-')
+            print(int(-r1_heading-))
             # -r2_heading-
-            print('-r2_heading-')
+            print(int(-r2_heading-))
             # -r1_velocity-
             # -r2_velocity-
             # -start_date-
