fd1ad387a25d48976e01e62fbc5e3723ac9efdea
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-04-21 22:13:04 -0500

    Update roboticsGUI.py

--- roboticsGUI.py
+++ roboticsGUI.py
@@ -650,7 +650,7 @@ def main():
         [sg.Input(size=(10,1), pad=((15,1),(1,1)), password_char = '*', key='-password-'), sg.Text(' Password')],
         [sg.Input(size=(10,1), pad=((15,1),(1,1)), key='-database-'), sg.Text(' Database')],
         [sg.Button("Connect to MySQL", size=(25,2), pad=((10,10),(15,15)), key="connect")],
-        [sg.Button("Debug Fill", size=(10,1), pad=((15,1),(5,5)), key="fill"), sg.Button("Without MySQL", size=(15,1), pad=((1,10),(5,5)), key="open")],
+        [sg.Button("Debug Fill", size=(13,1), pad=((10,2),(5,5)), key="fill"), sg.Button("Without MySQL", size=(13,1), pad=((2,10),(5,5)), key="open")],
         [sg.Text('')],
         [sg.Text('Please connect to MySQL.', key="-output-")]
     ]
