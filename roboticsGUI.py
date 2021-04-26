aef5a143feeb2f1ceaaeff9f68b0b46e95222ae3
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-04-26 18:07:36 -0500

    Update roboticsGUI.py

--- roboticsGUI.py
+++ roboticsGUI.py
@@ -781,10 +781,10 @@ def main():
                 continue
 
         elif event == "fill":
-            window['-hostname-'].update('localhost')
-            window['-username-'].update('elk')
-            window['-password-'].update('password')
-            window['-database-'].update('test')
+			window['-hostname-'].update('localhost')
+            window['-username-'].update('root')
+            window['-password-'].update('')
+            window['-database-'].update('robot_control')
         
     window.close()
 if __name__ == "__main__":
