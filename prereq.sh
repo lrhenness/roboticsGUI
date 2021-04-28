7711ba218cbf142f6866039c153d569ddcce1e7d
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-04-28 11:57:20 -0500

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -2,15 +2,10 @@
 
 # Check if sudo
 if [[ "$EUID" = 0 ]]; then
-    echo "(1) already root"
+    echo "already root"
 else
-    sudo -k # make sure to ask for password on next sudo
-    if sudo true; then
-        echo "(2) correct password"
-    else
-        echo "(3) wrong password"
-        exit 1
-    fi
+    echo "Please run this script with sudo privileges."
+    exit 1
 fi
 
 # MySQL section
