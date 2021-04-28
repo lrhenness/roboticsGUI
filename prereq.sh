9430f7e0638891c1413678098916dd3a1caeed2d
Luke Henness <48068359+lrhenness@users.noreply.github.com>
2021-04-28 10:48:24 -0500

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -5,3 +5,9 @@ apt install -y python3 python3-pip
 pip3 install pySimpleGUIWeb
 pip3 install mysql-connector-python
 pip3 install datetime
+echo "==================================="
+echo "==============SUCCESS!============="
+echo "==================================="
+echo "    You can now run the program    "
+echo "      python3 roboticsGUI.py       "
+echo "==================================="
